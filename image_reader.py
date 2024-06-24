902c5f74a387dfff35ff8b0dcc9a12f8dfaf6334
Eddie Stump <ejstump13@gmail.com>
2024-06-24 16:04:46 -0400

    Refactored get_text_from_image method into ImageReader class

--- image_reader.py
+++ image_reader.py
@@ -1,8 +1,47 @@
 import json
 import os
+from PIL import Image
 
 
 class ImageReader:
+
+    # takes in an image, returns a list of the lines of text in the image
+    def get_text_from_image(self, img_str):
+        img = Image.open(img_str)
+        img = img.crop((520, 60, 520 + 400, 60 + 27 * 16))
+        # default scale factor, number of pixels per minecraft pixel
+        # scale_factor = 6 if RETINA_DISPLAY else 3
+        scale_factor = 3
+
+        ign_return_list = []
+
+        # repeats 16 times as that is the maximum number of players in a lobby
+        for x in range(16):
+            temp_img = img.crop(box=(0, 9 * scale_factor * x, img.width, 9 * scale_factor * (x + 1) - scale_factor))
+
+            arr = []
+
+            for i in range(int(temp_img.width / scale_factor)):
+                arr.append([])
+                for k in range(int(temp_img.height / scale_factor)):
+                    # plus ones move to the center roughly
+                    px = temp_img.getpixel(xy=(i * scale_factor + 1, k * scale_factor + 1))
+
+                    # the 4 rank colors, all of which may appear in tab
+                    acceptable_colors = [(255, 170, 0), (85, 255, 255), (85, 255, 85), (170, 170, 170)]
+
+                    # if the given pixel is one of these colors, it is part of a character
+                    if acceptable_colors.__contains__(px):
+                        arr[i].append(1)
+                    else:
+                        arr[i].append(0)
+
+            # strings of length 0 aren't usernames
+            temp_ign = self.read_string(arr)
+            if len(temp_ign) > 0:
+                ign_return_list.append(temp_ign)
+
+        return ign_return_list
 
     def __init__(self):
         # reading the key from a file
